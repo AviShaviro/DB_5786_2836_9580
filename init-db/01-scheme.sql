af39bd03f7df94de0cc47539b7021da6779426d4
shraga77 <shragachesrak@gmail.com>
2026-04-11 22:15:17 +0300

    change scheme

--- init-db/01-scheme.sql
+++ init-db/01-scheme.sql
@@ -1,59 +1,76 @@
-CREATE SCHEMA IF NOT EXISTS "public";
-
-CREATE TABLE "public"."Chapters" (
-    "chapter_id" serial NOT NULL,
-    "course_id" int NOT NULL,
-    "chapter_order" int NOT NULL,
-    "title" varchar(255) NOT NULL,
-    "content_text" text,
-    PRIMARY KEY ("chapter_id")
+CREATE TABLE USERS
+(
+  user_id SERIAL NOT NULL,
+  PRIMARY KEY (user_id)
 );
 
-CREATE TABLE "public"."Course_Progress" (
-    "user_id" int NOT NULL,
-    "chapter_id" int NOT NULL,
-    "is_completed" boolean,
-    "completion_date" date,
-    PRIMARY KEY ("user_id", "chapter_id")
+CREATE TABLE COURSES
+(
+  course_id SERIAL PRIMARY KEY,
+  title VARCHAR(255) NOT NULL,
+  description TEXT,
+  publish_date DATE NOT NULL,
+  user_id INT NOT NULL,
+  FOREIGN KEY (user_id) REFERENCES USERS(user_id)
 );
 
-CREATE TABLE "public"."Courses" (
-    "course_id" serial NOT NULL,
-    "title" varchar(255) NOT NULL,
-    "creator_user_id" int NOT NULL,
-    "publish_date" date,
-    PRIMARY KEY ("course_id")
+CREATE TABLE CHAPTERS
+(
+  chapter_id SERIAL PRIMARY KEY,
+  title VARCHAR(255) NOT NULL,
+  content_text TEXT,
+  chapter_order INT NOT NULL,
+  course_id INT NOT NULL,
+  FOREIGN KEY (course_id) REFERENCES COURSES(course_id)
 );
 
-CREATE TABLE "public"."Puzzle_Attempts" (
-    "attempt_id" serial NOT NULL,
-    "puzzle_id" int NOT NULL,
-    "user_id" int NOT NULL,
-    "is_successful" boolean,
-    "time_taken_sec" int,
-    "attempt_date" timestamp DEFAULT CURRENT_TIMESTAMP,
-    PRIMARY KEY ("attempt_id")
+CREATE TABLE TAGS
+(
+  tag_id SERIAL PRIMARY KEY,
+  tag_name VARCHAR(100) NOT NULL UNIQUE,
+  description TEXT,
 );
 
-CREATE TABLE "public"."Puzzles" (
-    "puzzle_id" serial NOT NULL,
-    "fen_string" varchar(255) NOT NULL,
-    "solution_moves" varchar(255) NOT NULL,
-    "difficulty_elo" int,
-    "primary_tag_id" int,
-    PRIMARY KEY ("puzzle_id")
+CREATE TABLE PUZZLES
+(
+  puzzle_id SERIAL PRIMARY KEY,
+  fen_string VARCHAR(255) NOT NULL,
+  solution_moves VARCHAR(255) NOT NULL,
+  difficulty_elo INT,
+  tag_id INT,
+  FOREIGN KEY (tag_id) REFERENCES TAGS(tag_id)
 );
 
-CREATE TABLE "public"."Tags" (
-    "tag_id" serial NOT NULL,
-    "tag_name" varchar(100) NOT NULL UNIQUE,
-    "description" text,
-    PRIMARY KEY ("tag_id")
+CREATE TABLE DAILY_PUZZLES
+(
+  daily_puzzle_id SERIAL PRIMARY KEY,
+  puzzle_id INT NOT NULL,
+  puzzle_date DATE NOT NULL UNIQUE,
+  title VARCHAR(255) NOT NULL,
+  bonus_xp INT DEFAULT 10,
+  FOREIGN KEY (puzzle_id) REFERENCES PUZZLES(puzzle_id)
 );
 
--- Foreign key constraints
--- Schema: public
-ALTER TABLE "public"."Puzzles" ADD CONSTRAINT "fk_Puzzles_primary_tag_id_Tags_tag_id" FOREIGN KEY("primary_tag_id") REFERENCES "public"."Tags"("tag_id");
-ALTER TABLE "public"."Puzzle_Attempts" ADD CONSTRAINT "fk_Puzzle_Attempts_puzzle_id_Puzzles_puzzle_id" FOREIGN KEY("puzzle_id") REFERENCES "public"."Puzzles"("puzzle_id");
-ALTER TABLE "public"."Chapters" ADD CONSTRAINT "fk_Chapters_course_id_Courses_course_id" FOREIGN KEY("course_id") REFERENCES "public"."Courses"("course_id");
-ALTER TABLE "public"."Course_Progress" ADD CONSTRAINT "fk_Course_Progress_chapter_id_Chapters_chapter_id" FOREIGN KEY("chapter_id") REFERENCES "public"."Chapters"("chapter_id");
+CREATE TABLE COURSE_PROGRESS
+(
+    course_progress_id SERIAL PRIMARY KEY,
+  user_id INT NOT NULL,
+  course_id INT NOT NULL,
+  is_completed BOOLEAN DEFAULT FALSE,
+  start_date DATE NOT NULL,
+  completion_date DATE,
+  FOREIGN KEY (user_id) REFERENCES USERS(user_id),
+  FOREIGN KEY (course_id) REFERENCES COURSES(course_id)
+);
+
+CREATE TABLE PUZZLE_ATTEMPT
+(
+  attempt_id SERIAL PRIMARY KEY,
+  user_id INT NOT NULL,
+  puzzle_id INT NOT NULL,
+  is_successful BOOLEAN NOT NULL,
+  time_taken_sec INT NOT NULL,
+  attempt_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  FOREIGN KEY (puzzle_id) REFERENCES PUZZLES(puzzle_id),
+  FOREIGN KEY (user_id) REFERENCES USERS(user_id)
+);
